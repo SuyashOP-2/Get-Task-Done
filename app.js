d47c4ba48b3bbe78ca25caa7cf8a3b0ea4de141f
Suyash <=>
2023-02-15 18:50:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const express = require('express')
+const app = express();
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//midleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+//routes
+app.use('/api/v1/tasks', tasks) 
+
+const port = process.env.PORT || 3001
+
+const start = async () => {
+    try{
+      await connectDB(process.env.MONGO_URI)
+      app.listen(port, console.log(`server is listening on port ${port}`))
+      
+    }
+    catch (error){
+
+        console.log(error)
+
+    }
+}
+
+start()
+
+
+
+
+
---
+++ db/connect.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
+
+
+const connectDB = (url) => {
+
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+        
+         
+    })
+
+}
+
+module.exports = connectDB
+
+
+ 
